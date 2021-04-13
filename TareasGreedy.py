902046fbde1c192354782668e2d4286bc9a3e443
Ignacio Cassi <nacho.cassi97@gmail.com>
2021-04-12 23:43:18 -0300

    TareasGreedy v2

--- TareasGreedy.py
+++ TareasGreedy.py
@@ -13,22 +13,43 @@ class tarea:
     def getTiempo(self):
         return self.tiempo
 
-tarea1=tarea(1,50,2)
-tarea2=tarea(2,10,1)
-tarea3=tarea(3,15,2)
-tarea4=tarea(4,30,1)
-tareas=[tarea1,tarea2,tarea3,tarea4]
+def generarTareas():
+    tareas=[]
+    tarea1=tarea(1,50,2)
+    tarea2=tarea(2,10,1)
+    tarea3=tarea(3,15,2)
+    tarea4=tarea(4,30,1)
+    tareas=[tarea1,tarea2,tarea3,tarea4]
+    return tareas
 
-etapa=1
-secuencia=[]
-mayorGanancia=0
-while (etapa<=len(tareas)): #max(tiempoMayorTarea)
+def ordenarTareas(tareas):
     for i in range(len(tareas)):
-        if (tareas[i].getTiempo()<=etapa) and (tareas[i].getGanancia()>mayorGanancia):
-            numeroMayorGanancia=tareas[i].getNumero()
-            mayorGanancia=tareas[i].getGanancia()
-            tiempoMayorGanancia=tareas[i].getTiempo()
-    secuencia.append((numeroMayorGanancia,mayorGanancia,tiempoMayorGanancia))
-    tareas.pop(i)
-    etapa+=1
-print(secuencia)
+        for j in range(i+1,len(tareas)):
+            if (tareas[j].getGanancia()>tareas[i].getGanancia()):
+                aux=tareas[j]
+                tareas[j]=tareas[i]
+                tareas[i]=aux
+
+def obtenerSecuencia(tareas):
+    etapa=1
+    secuencia=[]
+    mayorGanancia=0
+    while (etapa<=len(tareas)):
+        for i in range(len(tareas)):
+            if (tareas[i].getTiempo()<=etapa) and (tareas[i].getGanancia()>mayorGanancia):
+                numeroMayorGanancia=tareas[i].getNumero()
+                mayorGanancia=tareas[i].getGanancia()
+                tiempoMayorGanancia=tareas[i].getTiempo()
+        secuencia.append((numeroMayorGanancia,mayorGanancia,tiempoMayorGanancia))
+        tareas.pop(i)
+        etapa+=1
+    return secuencia
+
+def __main__():
+    tareas=generarTareas()
+    ordenarTareas(tareas)
+    secuencia=obtenerSecuencia(tareas)
+    print(secuencia)
+
+if __name__=="__main__":
+    __main__()
